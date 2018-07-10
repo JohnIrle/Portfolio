c1094f7ad30e4d10732cb2c1d39f413748112654
JohnIrle <irlejohn@gmail.com>
2018-07-09 23:30:13 -0500

    Change to functional component and update class to className.

--- portfolio-react/src/components/Navbar.js
+++ portfolio-react/src/components/Navbar.js
@@ -1,50 +1,48 @@
 import React from "react";
 
-class Navbar extends React.Component {
-  render() {
-    return (
-      <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <button
-              type="button"
-              class="navbar-toggle"
-              data-toggle="collapse"
-              data-target="#navbar-collapse-main"
-            >
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar" />
-              <span class="icon-bar" />
-              <span class="icon-bar" />
-            </button>
-            <a class="navbar-brand" href="http://johnirle.com">
-              John Irle
-            </a>
-          </div>
-
-          <div class="collapse navbar-collapse" id="navbar-collapse-main">
-            <ul class="nav navbar-nav navbar-right">
-              <li>
-                <a href="https://github.com/JohnIrle">Github</a>
-              </li>
-              <li>
-                <a href="#about">About</a>
-              </li>
-              <li>
-                <a href="#portfolio">Portfolio</a>
-              </li>
-              <li>
-                <a href="#contact">Contact</a>
-              </li>
-              <li>
-                <a href="http://johnirle.com/blog/">Blog</a>
-              </li>
-            </ul>
-          </div>
+function Navbar(props) {
+  return (
+    <nav className="navbar navbar-default navbar-fixed-top">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle"
+            data-toggle="collapse"
+            data-target="#navbar-collapse-main"
+          >
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+          </button>
+          <a className="navbar-brand" href="http://johnirle.com">
+            John Irle
+          </a>
         </div>
-      </nav>
-    );
-  }
+
+        <div className="collapse navbar-collapse" id="navbar-collapse-main">
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <a href="https://github.com/JohnIrle">Github</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#portfolio">Portfolio</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+            <li>
+              <a href="http://johnirle.com/blog/">Blog</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
