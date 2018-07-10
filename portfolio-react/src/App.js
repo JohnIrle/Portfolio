725358a5e4f731cc53aaf4bdfefc8d70fa41f734
JohnIrle <irlejohn@gmail.com>
2018-07-09 23:31:16 -0500

    Import Navbar component and render.

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 class App extends Component {
   render() {
-    return <div />;
+    return <Navbar />;
   }
 }
 
