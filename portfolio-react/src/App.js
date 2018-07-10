6a135d687471d5f9f9883cc7ad2f9cf332a7816b
JohnIrle <irlejohn@gmail.com>
2018-07-10 18:44:25 -0500

    Import and structure Components.

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
+import "./resources/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
-    return <Navbar />;
+    return (
+      <div>
+        <Navbar />
+        <Home />
+        <About />
+        <Portfolio />
+        <Contact />
+        <hr />
+        <Footer />
+      </div>
+    );
   }
 }
 
