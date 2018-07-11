f84601fb1df1f84822cc4d33ca14e8d93d33b730
JohnIrle <irlejohn@gmail.com>
2018-07-11 17:20:29 -0500

    Remove form to ContactForm component.

--- portfolio-react/src/components/Contact.js
+++ portfolio-react/src/components/Contact.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ContactForm from "./ContactForm";
 
 function Contact(props) {
   return (
@@ -12,49 +13,7 @@ function Contact(props) {
             <div id="form-messages" />
           </div>
         </div>
-        <form id="ajax-contact" method="post" action="mailer.php">
-          <div className="form-group">
-            <div className="row">
-              <div className="form-group col-xs-12 col-sm-12 col-md-6 col-md-offset-3 col-lg-6 col-lg-offset-3 ">
-                <label htmlFor="name">Name:</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="name"
-                  name="name"
-                  required
-                />
-              </div>
-            </div>
-            <div className="row">
-              <div className="form-group col-xs-12 col-sm-12 col-md-6 col-md-offset-3 col-lg-6 col-lg-offset-3 ">
-                <label htmlFor="email">Email:</label>
-                <input
-                  className="form-control"
-                  type="email"
-                  id="email"
-                  name="email"
-                  required
-                />
-              </div>
-            </div>
-            <div className="row">
-              <div className="form-group col-xs-12 col-sm-12 col-md-6 col-md-offset-3 col-lg-6 col-lg-offset-3">
-                <label htmlFor="message">Message:</label>
-                <textarea
-                  className="form-control"
-                  id="message"
-                  name="message"
-                  required
-                />
-                <button className="btn btn-default form-control" type="submit">
-                  Send
-                </button>
-              </div>
-            </div>
-          </div>
-        </form>
-
+        <ContactForm />
         <hr />
 
         <p className="lead">Or contact me on these sites.</p>
