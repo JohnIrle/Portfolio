a8ad342c05d5875169f32313ca745bbe72c88001
JohnIrle <irlejohn@gmail.com>
2018-07-11 20:09:49 -0500

    Add react-dom-confetti config and logic. Work on handleSubmit().

--- portfolio-react/src/components/ContactForm.js
+++ portfolio-react/src/components/ContactForm.js
@@ -1,9 +1,43 @@
 import React from "react";
+import Confetti from "react-dom-confetti";
 
 class ContactForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { confetti: false };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange() {}
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    this.setState({ confetti: true });
+    const data = new FormData(e.target);
+
+    // fetch("../mailer.php", {
+    //   method: "post",
+    //   body: data
+    // });
+
+    console.log(data);
+  }
+
   render() {
+    const config = {
+      angle: 90,
+      spread: 223,
+      startVelocity: 45,
+      elementCount: 75,
+      decay: 0.9
+    };
+
     return (
-      <form id="ajax-contact" method="post" action="mailer.php">
+      <form id="ajax-contact" onSubmit={this.handleSubmit}>
         <div className="form-group">
           <div className="row">
             <div className="form-group col-xs-12 col-sm-12 col-md-6 col-md-offset-3 col-lg-6 col-lg-offset-3 ">
@@ -40,6 +74,7 @@ class ContactForm extends React.Component {
                 name="message"
                 required
               />
+              <Confetti active={this.state.confetti} config={config} />
               <button className="btn btn-default form-control" type="submit">
                 Send
               </button>
