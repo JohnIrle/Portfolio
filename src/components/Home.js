aa7b58d70cbeba34717bda6ab37c2cfd8c62e6c1
JohnIrle <irlejohn@gmail.com>
2019-10-20 20:56:20 -0500

    Update fontawesome color

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,11 +14,11 @@ function Home(props) {
           <hr />
           <div className="social">
             <ul className="inline">
-              <li><a href="https://twitter.com/irlejohn"><FontAwesomeIcon icon={faTwitter} size="2x" /></a></li>
-              <li><a href="http://github.com/JohnIrle"><FontAwesomeIcon icon={faGithub} size="2x" /></a></li>
-              <li><a href="https://www.linkedin.com/in/johnirle"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a></li>
-              <li><a href="http://stackoverflow.com/users/4778613/doudeman"><FontAwesomeIcon icon={faStackOverflow} size="2x" /></a></li>
-              <li><a href="https://www.freecodecamp.com/johnirle"><FontAwesomeIcon icon={faFreeCodeCamp} size="2x" /></a></li>
+              <li><a href="https://twitter.com/irlejohn"><FontAwesomeIcon icon={faTwitter} size="2x" color="gray" /></a></li>
+              <li><a href="http://github.com/JohnIrle"><FontAwesomeIcon icon={faGithub} size="2x" color="gray" /></a></li>
+              <li><a href="https://www.linkedin.com/in/johnirle"><FontAwesomeIcon icon={faLinkedin} size="2x" color="gray" /></a></li>
+              <li><a href="http://stackoverflow.com/users/4778613/doudeman"><FontAwesomeIcon icon={faStackOverflow} size="2x" color="gray" /></a></li>
+              <li><a href="https://www.freecodecamp.com/johnirle"><FontAwesomeIcon icon={faFreeCodeCamp} size="2x" color="gray" /></a></li>
             </ul>
           </div>
         </div>
