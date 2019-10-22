46f404ac8c8cbdb859a9e2762fa96df767792090
JohnIrle <irlejohn@gmail.com>
2019-10-22 12:05:40 -0500

    Add Contact Section

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <About />
       <Projects />
       <hr />
+      <Contact />
       <Footer />
     </>
   );
