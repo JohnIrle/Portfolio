a63bd8e989f0e7c45ea3560f56c383badce724a0
JohnIrle <irlejohn@gmail.com>
2019-10-22 13:52:09 -0500

    Change class to className

--- src/components/About.js
+++ src/components/About.js
@@ -6,17 +6,17 @@ function About() {
     <section id="about">
       <Container>
         <h2>About</h2>
-        <p class="lead">
+        <p className="lead">
           I'm a Computer Science Graduate Student and Freelance web developer.
 
           My interests include web development, cyber security and machine learning.
         </p>
 
-        <p class="lead">
+        <p className="lead">
           My focus is on JavaScript(React, Express, Node, MongoDB), and Python.
         </p>
 
-        <p class="lead">Want to work together? I would love to hear from you.
+        <p className="lead">Want to work together? I would love to hear from you.
         </p>
       </Container>
     </section>
