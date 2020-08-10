f5128cbd8ff5eadf64c4878f4cbedf9088e37ae2
JohnIrle <irlejohn@gmail.com>
2020-08-10 00:08:23 -0500

    Change to functional component and useState hook.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,99 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { Button, Container, Form, Row, Col } from "react-bootstrap"
 
 import SocialIcons from "./SocialIcons";
 
-class Contact extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      email: '',
-      message: '',
-      mailSent: false,
-      error: null
-    }
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
-  }
+const Contact = (props) => {
 
-  handleFormSubmit(event) {
-    event.preventDefault();
-    axios({
-      method: 'post',
-      url: 'mailer.php',
-      data: this.state
-    })
-      .then(result => {
-        this.setState({
-          mailSent: result.data.sent,
-          name: '',
-          email: '',
-          message: ''
-        })
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+    mailSent: false,
+    error: null
+  })
+
+  const {
+    name,
+    email,
+    message,
+    mailSent,
+    error
+  } = formData;
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const result = await axios.post("mailer.php", formData)
+      setFormData({
+        mailSent: result.data.sent,
+        name: '',
+        email: '',
+        message: ''
       })
-      .catch(error => this.setState({ error: error.message }))
+    } catch (error) {
+      setFormData({ error: error.message })
+    }
   }
 
-  render() {
-    return (
-      <section id="contact">
-        <Container>
-          <Row>
-            <Col className="text-center">
-              <h2>Contact me</h2>
-              <p className="lead">Send me an Email</p>
+  return (
+    <section id="contact">
+      <Container>
+        <Row>
+          <Col className="text-center">
+            <h2>Contact me</h2>
+            <p className="lead">Send me an Email</p>
 
-              <div>
-                {this.state.mailSent &&
-                  <div>Thank you for contacting me.</div>
-                }
-              </div>
-              <div>
-                {this.state.error &&
-                  <div>{this.state.error}</div>
-                }
-              </div>
-            </Col>
+            <div>
+              {mailSent &&
+                <div>Thank you for contacting me.</div>
+              }
+            </div>
+            <div>
+              {error &&
+                <div>{error}</div>
+              }
+            </div>
+          </Col>
 
-          </Row>
-          <Form id="ajax-contact" method="post" action="mailer.php">
-            <Form.Group>
+        </Row>
+        <Form id="ajax-contact" method="post" action="mailer.php">
+          <Form.Group>
 
-              <Form.Label>Name:</Form.Label>
-              <Form.Control type="text" id="name" name="name" required
-                value={this.state.name}
-                onChange={e => this.setState({ name: e.target.value })}
-              />
-
-
-              <Form.Label>Email:</Form.Label>
-              <Form.Control type="email" id="email" name="email" required
-                value={this.state.email}
-                onChange={e => this.setState({ email: e.target.value })}
-              />
+            <Form.Label>Name:</Form.Label>
+            <Form.Control type="text" id="name" name="name" required
+              value={name}
+              onChange={e => setFormData({ name: e.target.value })}
+            />
 
 
-              <Form.Label>Message:</Form.Label>
-              <Form.Control as="textarea" id="message" name="message" required
-                value={this.state.message}
-                onChange={e => this.setState({ message: e.target.value })}
-              />
-              <Button variant="secondary" type="submit" onClick={e => this.handleFormSubmit(e)}>Send</Button>
+            <Form.Label>Email:</Form.Label>
+            <Form.Control type="email" id="email" name="email" required
+              value={email}
+              onChange={e => setFormData({ email: e.target.value })}
+            />
 
-            </Form.Group>
-          </Form>
 
-          <hr />
+            <Form.Label>Message:</Form.Label>
+            <Form.Control as="textarea" id="message" name="message" required
+              value={message}
+              onChange={e => setFormData({ message: e.target.value })}
+            />
+            <Button variant="secondary" type="submit" onClick={e => handleFormSubmit(e)}>Send</Button>
 
-          <p className="lead">Or contact me on these sites.</p>
+          </Form.Group>
+        </Form>
 
-          <SocialIcons />
-        </Container>
-      </section>
+        <hr />
 
-    )
-  }
+        <p className="lead">Or contact me on these sites.</p>
+
+        <SocialIcons />
+      </Container>
+    </section>
+
+  )
 }
 
 export default Contact;
