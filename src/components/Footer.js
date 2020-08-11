b12d469542938148d1243f052dac9c145f9e3d1f
JohnIrle <irlejohn@gmail.com>
2020-08-10 19:46:00 -0500

    Add Date.GetFullYear()

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
 function Footer(props) {
-  return (
-    <footer className="footer">
-      <div className="container">
-        <div className="navbar-text pull-left">
-          <p>Copyright &copy; John Irle 2020</p>
-        </div>
-      </div>
-    </footer>
-  );
+    return (
+        <footer className="footer">
+            <div className="container">
+                <div className="navbar-text pull-left">
+                    <p>Copyright &copy; John Irle {new Date().getFullYear()}</p>
+                </div>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
