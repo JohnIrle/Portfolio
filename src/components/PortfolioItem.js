d952af6ed1cf57a1ec1363fc19b7e296ae03650a
JohnIrle <irlejohn@gmail.com>
2020-08-10 19:47:14 -0500

    Add button links to code and demo and name.

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import { Col, Image } from 'react-bootstrap'
+import React from "react";
+import { Col, Image, Button } from "react-bootstrap";
 
 function PortfolioItem(props) {
-  return (
-    <Col span={"auto"} lg={true}>
-      < a href={props.url} >
-        <Image src={props.img} alt="props.alt" fluid />
-      </a >
-    </Col >
-  );
+    return (
+        <Col span={"auto"} lg={true} style={{ marginTop: "3%" }}>
+            <h5 className="portfolio-items-link">{props.name}</h5>
+            <a href={props.url}>
+                <Image src={props.img} alt="props.alt" fluid />
+            </a>
+            <div className="portfolio-items-link">
+                {props.url && (
+                    <Button href={props.url} variant="outline-dark" size="sm">
+                        Demo
+                    </Button>
+                )}
+                {props.github && (
+                    <Button
+                        href={props.github}
+                        variant="outline-dark"
+                        size="sm"
+                    >
+                        Code
+                    </Button>
+                )}
+            </div>
+        </Col>
+    );
 }
 
 export default PortfolioItem;
