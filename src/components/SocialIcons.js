5b59b1d57992095e9f50839fd504d3622db4d7b7
JohnIrle <irlejohn@gmail.com>
2020-08-10 19:46:33 -0500

    Add Brand colors to icons.

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -1,19 +1,65 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter, faGithub, faLinkedin, faStackOverflow, faFreeCodeCamp } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faTwitter,
+    faGithub,
+    faLinkedin,
+    faStackOverflow,
+    faFreeCodeCamp,
+} from "@fortawesome/free-brands-svg-icons";
 
 function SocialIcons(props) {
-  return (
-    <div className="social">
-      <ul className="inline">
-        <li><a href="https://twitter.com/irlejohn"><FontAwesomeIcon icon={faTwitter} size="2x" color="gray" /></a></li>
-        <li><a href="http://github.com/JohnIrle"><FontAwesomeIcon icon={faGithub} size="2x" color="gray" /></a></li>
-        <li><a href="https://www.linkedin.com/in/johnirle"><FontAwesomeIcon icon={faLinkedin} size="2x" color="gray" /></a></li>
-        <li><a href="http://stackoverflow.com/users/4778613/doudeman"><FontAwesomeIcon icon={faStackOverflow} size="2x" color="gray" /></a></li>
-        <li><a href="https://www.freecodecamp.com/johnirle"><FontAwesomeIcon icon={faFreeCodeCamp} size="2x" color="gray" /></a></li>
-      </ul>
-    </div>
-  )
+    return (
+        <div className="social">
+            <ul className="inline">
+                <li>
+                    <a href="https://twitter.com/irlejohn">
+                        <FontAwesomeIcon
+                            icon={faTwitter}
+                            size="2x"
+                            color="#3b5999"
+                        />
+                    </a>
+                </li>
+                <li>
+                    <a href="http://github.com/JohnIrle">
+                        <FontAwesomeIcon
+                            icon={faGithub}
+                            size="2x"
+                            color="#171515"
+                        />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/johnirle">
+                        <FontAwesomeIcon
+                            icon={faLinkedin}
+                            size="2x"
+                            color="#0077B5"
+                        />
+                    </a>
+                </li>
+                <li>
+                    <a href="http://stackoverflow.com/users/4778613/doudeman">
+                        <FontAwesomeIcon
+                            icon={faStackOverflow}
+                            size="2x"
+                            color="#f48024"
+                        />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.freecodecamp.com/johnirle">
+                        <FontAwesomeIcon
+                            icon={faFreeCodeCamp}
+                            size="2x"
+                            color="#006400"
+                        />
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
 }
 
 export default SocialIcons;
