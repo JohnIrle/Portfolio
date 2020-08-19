f59512b7149186190c02691c25428d9d94f56cfd
JohnIrle <irlejohn@gmail.com>
2020-08-19 07:16:59 -0500

    Remove machine learning and change cyber security to cybersecurity.

--- src/components/About.js
+++ src/components/About.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React from "react";
+import { Container } from "react-bootstrap";
 
 function About() {
-  return (
-    <section id="about">
-      <Container>
-        <h2>About</h2>
-        <p className="lead">
-          I'm a Computer Science Graduate Student and Freelance web developer.
+    return (
+        <section id="about">
+            <Container>
+                <h2>About</h2>
+                <p className="lead">
+                    I'm a Computer Science Graduate Student and Freelance web
+                    developer. My interests include front-end web development,
+                    back-end development and cybersecurity.
+                </p>
 
-          My interests include web development, cyber security and machine learning.
-        </p>
+                <p className="lead">
+                    My focus is on JavaScript(React, Express, Node, MongoDB),
+                    and Python.
+                </p>
 
-        <p className="lead">
-          My focus is on JavaScript(React, Express, Node, MongoDB), and Python.
-        </p>
-
-        <p className="lead">Want to work together? I would love to hear from you.
-        </p>
-      </Container>
-    </section>
-  );
+                <p className="lead">
+                    Want to work together? I would love to hear from you.
+                </p>
+            </Container>
+        </section>
+    );
 }
 
 export default About;
