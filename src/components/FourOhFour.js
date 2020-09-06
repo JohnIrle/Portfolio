3f193674e5005529eb008ef5968bcef0c960b735
JohnIrle <irlejohn@gmail.com>
2020-09-06 15:07:11 -0500

    Add div for background image

--- src/components/FourOhFour.js
+++ src/components/FourOhFour.js
@@ -4,12 +4,14 @@ import webb from "../resources/img/webb.png";
 
 const FourOhFour = () => {
     return (
-        <div className="container-fluid mt-5" style={{paddingTop: "4rem", backgroundImage: `url(${webb})`, backgroundRepeat: "repeat", height: "85vh"}}>
-            <div className="jumbotron text-center">
-                <h1>This page does not exist.</h1>
-                <p>Go back to the content?</p> 
-                <Link to="/"><button type="button" className="btn btn-dark mr-1">Portfolio</button></Link>
-                <Link to="/blog"><button type="button" className="btn btn-dark">Blog</button></Link>
+        <div style={{paddingTop: "4rem", backgroundImage: `url(${webb})`, backgroundRepeat: "repeat", height: "90vh"}}>
+            <div className="container mt-5" >
+                <div className="jumbotron text-center">
+                    <h1>This page does not exist.</h1>
+                    <p>Go back to the content?</p> 
+                    <Link to="/"><button type="button" className="btn btn-dark mr-1">Portfolio</button></Link>
+                    <Link to="/blog"><button type="button" className="btn btn-dark">Blog</button></Link>
+                </div>
             </div>
         </div>
     )
