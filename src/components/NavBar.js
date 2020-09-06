1520467898eac928cbb6736b59e303e8b4494c09
JohnIrle <irlejohn@gmail.com>
2020-09-06 14:35:46 -0500

    Change link from url to root.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap"
 
-function NavBar(props) {
+function NavBar() {
   return (
     <Navbar bg="light" expand="lg" fixed="top">
       <Navbar.Brand href="http://github.com/JohnIrle">John Irle</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="https://johnirle.com">Home</Nav.Link>
+          <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="#about">About</Nav.Link>
           <Nav.Link href="#portfolio">Portfolio</Nav.Link>
           <Nav.Link href="#contact">Contact</Nav.Link>
